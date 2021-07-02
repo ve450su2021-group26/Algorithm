be4060623797ddc45a393ad00c4ff0cf8579bb5a
junjzhang <junj.jay.zhang@gmail.com>
2021-07-02 15:56:02 +0800

    adjust the file structure

--- img_module/utils/tensor_operation.py
+++ img_module/utils/tensor_operation.py
@@ -7,13 +7,3 @@ def normalization_by_range(t):
     normalized_t = (t - min) / (max - min)
 
     return normalized_t
-
-
-from einops import rearrange
-
-
-def tensor_to_np(img_tensor):
-    img = img_tensor.mul(255).byte()
-    img = img.numpy()
-    img = rearrange(img, '(c h w -> c w h)')
-    return img
